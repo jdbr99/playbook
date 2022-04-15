7102d01867d98e5053b70ea3b3e3c62412d12559
David Bolaños <jd.bolanosrojas@ugto.mx>
2022-04-15 18:24:38 -0500

    All excercises done! ✅

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,13 @@
+export default class Pokemon {
+    constructor(name) {
+        this.name = name
+    }
+
+    sayHello() {
+        console.log(`Mi pokémon ${this.name} te saluda!!!`)
+    }
+
+    sayMessage(message) {
+        console.log(`Mi pokémon ${this.name} dice: ${message}`)
+    }
+}
---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,14 @@
+class Pokemon {
+    constructor(name) {
+        this.name = name
+    }
+
+    sayHello() {
+        console.log(`Mi pokémon ${this.name} te saluda!!!`)
+    }
+
+    sayMessage(message) {
+        console.log(`Mi pokémon ${this.name} dice: ${message}`)
+    }
+}
+module.exports = Pokemon
